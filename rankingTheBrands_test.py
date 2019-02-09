ec6a45eb1d4683554b5e5aee9e1598b7078c3aa1
timurcarstensen <timurcarstensen@Timurs-MacBook-Pro.local>
2019-02-09 22:21:32 +0900

    in the process of adapting to include headers in the request

--- rankingTheBrands_test.py
+++ rankingTheBrands_test.py
@@ -5,6 +5,9 @@ import pandas as pd
 import os
 import all_functions as fnc
 
+user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+headers={'User-Agent':user_agent}
+
 
 url_list = ['https://www.rankingthebrands.com/The-Brand-Rankings.aspx?rankingID=83&year=1200',
             'https://www.rankingthebrands.com/The-Brand-Rankings.aspx?rankingID=6&year=1214',
