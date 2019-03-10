f38a63c2ea485f10b158a69de9cb3c392c12a660
timurcarstensen <tm.carstensen@icloud.com>
2019-03-10 10:59:58 +0800

    changed over to vscode as main editor

--- reader.py
+++ reader.py
@@ -4,6 +4,15 @@ import functions as fnc  # importing functions file
 readIn = pd.read_excel('sorted.xlsx')  # reading in the sorted.xlsx file and creating a new DataFrame
 readIn = readIn.loc[:, ~readIn.columns.str.contains('^Unnamed')]
 
+
+try:
+    print(readIn.loc[(readIn.NAME == 'Amazon') & (readIn.YEAR == 2010) & (readIn.SOURCE == 'Interbrand')])
+except AttributeError:
+    print('wrong attribute')
+except Exception as e:
+    print(type(e))
+
+
 names = []
 yearList = []
 nameList = []
@@ -31,19 +40,27 @@ for i in readIn.SOURCE:  # finding all unique sources
     if i not in sources:
         sources.append(i)
 
-new_df = pd.DataFrame({'NAME': nameList, 'YEAR': yearList})  # creating a new DataFrame based upon brand names and all unique years
+df = pd.DataFrame({'NAME': nameList, 'YEAR': yearList})  # creating a new DataFrame based upon brand names and all unique years
 
-new_df['Country'] = 'NaN'  # adding a column for country
-new_df['Industry Sector'] = 'NaN'  # adding a column for industry sector
+df['Country'] = 'NaN'  # adding a column for country
+df['Industry Sector'] = 'NaN'  # adding a column for industry sector
 
 for i in sources:  # adding new columns for each source available
-    new_df[i] = 'NaN'
+    df[i] = 'NaN'
 
-columnList = list(new_df)
+columnList = list(df)
 
-currentBrand = str()
-currentYear = int()
-currentSource = str()
+# currentBrand = str()
+# currentYear = int()
+# currentSource = str()
+
+# for i in names:
+#     y = df.loc[df.NAME == i]
+#     cY = int()
+#     for i in y.YEAR:
+#         print(i)
 
 
 # fnc.toExcel('testing', new_df)
+
+
